c7977f5c7c0b6cfbc82afadbfb099fd62df8890e
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-05-17 19:57:17 -0500

    install react icons & add ui leading to course generation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { FaSquarePlus } from "react-icons/fa6";
 
 function Page() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex h-screen bg-zinc-100">
+      {/* Content Container*/}
+      <div className="p-28">
+        <div className="flex flex-col items-start gap-4">
+          <h2 className="text-3xl font-bold text-zinc-800">Your Courses</h2>
+          <p className="text-zinc-800">
+            Start by generating a new course or resuming where you left off!
+          </p>
+          <button className="flex items-center gap-3 rounded-xl border border-2 border-zinc-200 p-5 shadow-2xl">
+            <FaSquarePlus color={"lightGrey"} size={30} />
+            <p className="text-semibold text-zinc-800">Create a Course</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
