96ab66373f213dd65109666f97ace7a19ebf0331
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-05-18 13:06:14 -0500

    add basic styles to course creation ui

--- src/app/course/create/page.tsx
+++ src/app/course/create/page.tsx
@@ -2,8 +2,15 @@ import React from "react";
 
 function Page() {
   return (
-    <div>
-      <p>Course creation page</p>
+    <div className="flex h-screen w-full flex-col items-center bg-zinc-100 pt-32">
+      <div className="flex flex-col justify-center gap-3">
+        <p className="text-zinc-800">Course creation page</p>
+        <p className="text-zinc-800">&quot;Text about what to do&quot;</p>
+        <input
+          placeholder="enter topic"
+          className="rounded-lg bg-gray-300 px-4 py-2"
+        />
+      </div>
     </div>
   );
 }
