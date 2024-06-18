0cab9a72890329d86db74a0cb867ecc02aa00f52
eddie-kuo-fpc <eddiekuo97@gmail.com>
2024-06-18 10:26:13 -0500

    transfer course unit and chapter prototype

--- src/lib/gpt.ts
+++ src/lib/gpt.ts
@@ -14,29 +14,65 @@ export async function strict_output(
   output_format: OutputFormat,
   model: string = "gpt-3.5-turbo",
 ) {
-  let output_format_prompt = `\nOutput in the following json string format: " + ${JSON.stringify(output_format)} + "\nBe concise.`;
+  let output_format_prompt = `\nOutput in the following json string format: " + ${JSON.stringify(output_format)} + "\nBe concise. \nDo not put quotation marks or escape character \\ in the output fields.`;
 
   let my_system_prompt = `${JSON.stringify(system_prompt)} + ${JSON.stringify(output_format_prompt)}`;
 
-  const response = await openai.chat.completions.create({
-    temperature: 1,
-    model: model,
-    messages: [
-      {
-        role: "system",
-        content: my_system_prompt,
-      },
-      { role: "user", content: user_prompt.toString() },
-    ],
-  });
+  try {
+    const response = await openai.chat.completions.create({
+      temperature: 1,
+      model: model,
+      messages: [
+        {
+          role: "system",
+          content: my_system_prompt,
+        },
+        { role: "user", content: user_prompt.toString() },
+      ],
+    });
 
-  let res: string =
-    response.choices[0].message?.content?.replace(/'/g, '"') ?? "";
+    let res: string =
+      response.choices[0].message?.content?.replace(/'/g, '"') ?? "";
 
-  // ensure that we don't replace away apostrophes in text
-  res = res.replace(/(\w)"(\w)/g, "$1'$2");
+    if (res.length) {
+      // remove leading and unwanted backslashes
+      res = res.replace(/\\/g, "");
+      // ensure that we don't replace away apostrophes in text
+      res = res.replace(/(\w)"(\w)/g, "$1'$2");
 
-  let output: any = JSON.parse(res);
+      let output: any = JSON.parse(res);
 
-  return output;
+      return output;
+    }
+
+    return res;
+  } catch (error) {
+    console.log("ERROR: Error when generating with OpenAI:", error);
+    return {};
+  }
 }
+
+export async function generateSummary(
+  system_prompt: string,
+  user_prompt: string,
+  model: string = "gpt-3.5-turbo",
+) {
+  try {
+    const response = await openai.chat.completions.create({
+      temperature: 1,
+      model: model,
+      messages: [
+        {
+          role: "system",
+          content: system_prompt,
+        },
+        { role: "user", content: user_prompt.toString() },
+      ],
+    });
+
+    return response.choices[0].message.content;
+  } catch (error) {
+    return "";
+  }
+}
+
