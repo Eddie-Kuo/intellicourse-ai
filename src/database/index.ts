495f934f7a02b0f20ab49b920b99e432e8107df7
eddie-kuo-fpc <eddiekuo97@gmail.com>
2024-06-28 15:26:19 -0500

    setup env with new database fields

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 const client = createClient({
-  url: "DATABASE_URL",
-  authToken: "DATABASE_AUTH_TOKEN",
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN!,
 });
 export const db = drizzle(client);
