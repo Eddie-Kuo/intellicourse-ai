e2cd4eac381a83fe4ba9c5610cf31f5ab125b40a
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-07-04 01:06:12 -0500

    fix pub route

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/api/webhooks/clerk/",
+  "/api/webhooks/clerk",
 ]);
 
 export default clerkMiddleware((auth, request) => {
