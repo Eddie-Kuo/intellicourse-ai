f8898a21c632d410bcab2fc7b3c7c2cc8cd0b0e7
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-07-11 17:43:00 -0500

    fetch unit for selected course

--- src/app/course/[...slug]/page.tsx
+++ src/app/course/[...slug]/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "@/database";
+import { getCourseDetails } from "@/database/queries/course";
+import { courses } from "@/database/schema/courses";
+import { eq } from "drizzle-orm";
 import React from "react";
 
 type PageProps = {
@@ -6,8 +10,12 @@ type PageProps = {
   };
 };
 
-export default function Page({ params: { slug } }: PageProps) {
+export default async function Page({ params: { slug } }: PageProps) {
   const [courseId] = slug;
+
+  const course = await getCourseDetails(courseId);
+
+  console.log(course);
 
   return <div>Course: {courseId}</div>;
 }
--- src/database/queries/course.ts
+++ src/database/queries/course.ts
@@ -1,12 +1,25 @@
 import { eq } from "drizzle-orm";
 import { db } from "..";
 import { courses } from "../schema/courses";
+import { units } from "../schema/units";
 
 export async function getCourseList(userId: string) {
   const result = await db
     .select()
     .from(courses)
     .where(eq(courses.userId, userId));
+
+  return result;
+}
+
+// fetching the unit list for the selected course
+export async function getCourseDetails(courseId: string) {
+  const courseIdToNumber = parseInt(courseId);
+
+  const result = db
+    .select()
+    .from(units)
+    .where(eq(units.courseId, courseIdToNumber));
 
   return result;
 }
