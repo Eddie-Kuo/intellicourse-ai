162bf001898ac687ab71b78a746b3d2c424bbcba
eddie-kuo-fpc <eddiekuo97@gmail.com>
2024-07-17 14:26:05 -0500

    install shadcn components and init quiz section

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import { SelectChapter } from "@/database/schema/chapters";
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
 
 type QuizProps = {
   chapter: SelectChapter;
 };
 
 export default function Quiz({ chapter }: QuizProps) {
-  return <div className="text-darkText">{chapter.title}</div>;
+  return (
+    <div className="ml-8 mt-52 flex-[1]">
+      <h1 className="text-lg font-semibold">Concept Check</h1>
+
+      <div>
+        <h1 className="font-semibold">{chapter.question}</h1>
+        <div className="py-2"></div>
+      </div>
+      <Button onClick={() => {}} className="mt-2 w-full">
+        Check Answer
+      </Button>
+    </div>
+  );
 }
