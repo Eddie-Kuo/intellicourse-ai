8e3f5e778eaad8e713a013f35ec24909a5b4835c
eddie-kuo-fpc <eddiekuo97@gmail.com>
2024-07-17 13:19:41 -0500

    light change in course sidebar styling and spacing

--- src/components/CourseSideBar.tsx
+++ src/components/CourseSideBar.tsx
@@ -21,16 +21,14 @@ export default function CourseSideBar({
   const sortedUnits = courseDetails.units.sort((a, b) => a.unit - b.unit);
 
   return (
-    <div className="fixed flex h-full max-w-md flex-col justify-center rounded-r-3xl bg-zinc-200 p-6 sm:mt-0">
-      <h1 className="text-darkText text-3xl font-semibold uppercase">
+    <div className="fixed flex h-full max-w-md flex-col justify-center gap-5 rounded-r-3xl bg-zinc-200 p-6 sm:mt-0">
+      <h1 className="text-3xl font-semibold uppercase text-sky-800">
         {courseDetails.title}
       </h1>
       {sortedUnits.map((unit) => {
         return (
           <div key={unit.unit}>
-            <h2 className="text-darkText text-lg font-semibold">
-              {unit.title}
-            </h2>
+            <h2 className="text-lg font-semibold text-sky-800">{unit.title}</h2>
             {unit.chapters
               .sort((a, b) => a.chapter - b.chapter)
               .map((chapter) => {
@@ -44,6 +42,10 @@ export default function CourseSideBar({
                   >
                     <Link
                       href={`/course/${courseDetails.id}/${unit.unit}/${chapter.chapter}`}
+                      className={cn(
+                        "font-medium text-darkText",
+                        chapter.id === currentChapter && "text-white",
+                      )}
                     >
                       {chapter.title}
                     </Link>
