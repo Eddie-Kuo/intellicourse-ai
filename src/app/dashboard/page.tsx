fa1816e2bd591b121fca0eb8b3d036719de36534
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-07-31 10:06:32 -0500

    edit layout for dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,30 +15,32 @@ export default async function Page() {
   const data = await getCourseList(user.id);
 
   return (
-    <div className="flex bg-gradient-to-r from-zinc-200 to-slate-300">
-      {/* Content Container*/}
-      <div className="mx-auto w-full p-20">
-        <div className="flex flex-col items-start gap-4">
-          <h2 className="text-3xl font-bold text-zinc-800">Your Courses</h2>
-          <p className="text-zinc-800">
-            Start by generating a new course or resuming where you left off!
-          </p>
-          <Link
-            href="/create"
-            className="flex items-center gap-3 rounded-xl p-5 shadow-2xl ring-1 ring-slate-400"
-          >
-            <FaSquarePlus color={"lightGrey"} size={30} />
-            <p className="text-semibold text-zinc-800">Create a Course</p>
-          </Link>
-          <div className="grid grid-cols-3 gap-3 py-10 md:grid-cols-4 lg:grid-cols-5">
-            {data.length > 0 &&
-              data.map((course) => (
-                <CourseCard
-                  key={course.id}
-                  title={course.title}
-                  id={course.id}
-                />
-              ))}
+    <div className="h-full min-h-screen bg-gradient-to-r from-zinc-200 to-slate-300">
+      <div className="container relative mx-auto">
+        {/* Content Container*/}
+        <div className="py-20 md:px-10">
+          <div className="flex flex-col items-start gap-4">
+            <h2 className="text-3xl font-bold text-zinc-800">Your Courses</h2>
+            <p className="text-zinc-800">
+              Start by generating a new course or resuming where you left off!
+            </p>
+            <Link
+              href="/create"
+              className="flex items-center gap-3 rounded-xl p-5 shadow-2xl ring-1 ring-slate-400"
+            >
+              <FaSquarePlus color={"lightGrey"} size={30} />
+              <p className="text-semibold text-zinc-800">Create a Course</p>
+            </Link>
+            <div className="grid grid-cols-3 gap-3 py-10 md:grid-cols-4 lg:grid-cols-5">
+              {data.length > 0 &&
+                data.map((course) => (
+                  <CourseCard
+                    key={course.id}
+                    title={course.title}
+                    id={course.id}
+                  />
+                ))}
+            </div>
           </div>
         </div>
       </div>
