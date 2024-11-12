e15b110112c3e29547fb293cce47f91cc644a1b6
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-11-12 14:45:10 -0600

    remove vercel server function timeout

--- src/app/api/course/createCourse/route.ts
+++ src/app/api/course/createCourse/route.ts
@@ -20,9 +20,6 @@ interface courseOutput {
   }[];
 }
 
-export const maxDuration = 60;
-export const dynamic = "force-dynamic";
-
 export async function POST(request: Request) {
   const { ...body } = await request.json();
   const { topic, userId } = createCourseSchema.parse(body);
