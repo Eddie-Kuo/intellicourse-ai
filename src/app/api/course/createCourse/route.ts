8484760bfe033bb68b7dc743ceb27b658b8345ff
Eddie-Kuo <Eddiekuo97@gmail.com>
2024-11-12 11:47:36 -0600

    extend server functions

--- src/app/api/course/createCourse/route.ts
+++ src/app/api/course/createCourse/route.ts
@@ -20,6 +20,9 @@ interface courseOutput {
   }[];
 }
 
+export const maxDuration = 60;
+export const dynamic = "force-dynamic";
+
 export async function POST(request: Request) {
   const { ...body } = await request.json();
   const { topic, userId } = createCourseSchema.parse(body);
